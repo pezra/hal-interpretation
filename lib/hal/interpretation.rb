d888b1488f5fdaf534f490979bd7f3f6887ac415
Peter Williams <pezra@barelyenough.org>
2014-03-20 15:28:06 -0600

    initial commit

---
+++ lib/hal/interpretation.rb
@@ -0,0 +1,7 @@
+require "hal/interpretation/version"
+
+module Hal
+  module Interpretation
+    # Your code goes here...
+  end
+end
---
+++ lib/hal/interpretation/version.rb
@@ -0,0 +1,5 @@
+module Hal
+  module Interpretation
+    VERSION = "0.0.1"
+  end
+end
